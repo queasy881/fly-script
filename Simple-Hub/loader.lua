8b2e49a8eb80cb1a6d581495a5184fd475e5b1df
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:03:51 -0800

    Update loader.lua

--- Simple-Hub/loader.lua
+++ Simple-Hub/loader.lua
@@ -1,1 +1,24 @@
+-- SIMPLE HUB v3.7 Loader
+-- This is the ONLY file users execute with loadstring
+
+local BASE_URL = "https://raw.githubusercontent.com/queasy881/fly-script/main/"
+
+local function load(file)
+	return loadstring(game:HttpGet(BASE_URL .. file, true))()
+end
+
+-- Basic exploit safety check
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+-- Prevent double execution
+if _G.SIMPLE_HUB_LOADED then
+	warn("Simple Hub already loaded")
+	return
+end
+_G.SIMPLE_HUB_LOADED = true
+
+-- Load main client
+load("main.client.lua")
 
