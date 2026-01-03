289feb735e7f04d3f9eac041c116bcbc29e81859
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:24:23 -0800

    Update spinbot.lua

--- Simple-Hub/extra/spinbot.lua
+++ Simple-Hub/extra/spinbot.lua
@@ -1,1 +1,17 @@
+-- extra/spinbot.lua
+
+local SpinBot = {}
+
+SpinBot.enabled = false
+SpinBot.speed = 5
+local angle = 0
+
+function SpinBot.update(root, dt)
+	if not SpinBot.enabled or not root then return end
+	angle += dt * SpinBot.speed * 10
+	if angle > 360 then angle = 0 end
+	root.CFrame = CFrame.new(root.Position) * CFrame.Angles(0, math.rad(angle), 0)
+end
+
+return SpinBot
 
