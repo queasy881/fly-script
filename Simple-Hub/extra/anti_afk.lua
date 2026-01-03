d6005f3560874303f39b87c5aa3cfb4fec9c2de7
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:23:54 -0800

    Update anti_afk.lua

--- Simple-Hub/extra/anti_afk.lua
+++ Simple-Hub/extra/anti_afk.lua
@@ -1,1 +1,21 @@
+-- extra/anti_afk.lua
+
+local AntiAFK = {}
+
+AntiAFK.enabled = false
+local afkTime = 0
+
+function AntiAFK.update(dt)
+	if not AntiAFK.enabled then return end
+	afkTime += dt
+
+	if afkTime >= 15 then
+		afkTime = 0
+		local vu = game:GetService("VirtualUser")
+		vu:CaptureController()
+		vu:ClickButton2(Vector2.new())
+	end
+end
+
+return AntiAFK
 
