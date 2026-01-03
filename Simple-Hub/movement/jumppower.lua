c07cfad928701b8dbe31706e86b7b19789d78ee5
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:21:01 -0800

    Update jumppower.lua

--- Simple-Hub/movement/jumppower.lua
+++ Simple-Hub/movement/jumppower.lua
@@ -1,1 +1,17 @@
+-- movement/jumppower.lua
+
+local JumpPower = {}
+
+JumpPower.enabled = false
+JumpPower.value = 50
+
+function JumpPower.apply(humanoid)
+	if JumpPower.enabled then
+		humanoid.JumpPower = JumpPower.value
+	else
+		humanoid.JumpPower = 50
+	end
+end
+
+return JumpPower
 
