12c7cdac3778545d42e09afc409d1c5e50ae5129
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:18:47 -0800

    Update keybinds.lua

--- Simple-Hub/settings/keybinds.lua
+++ Simple-Hub/settings/keybinds.lua
@@ -1,1 +1,20 @@
+-- settings/keybinds.lua
+
+local Keybinds = {}
+
+-- Menu
+Keybinds.ToggleMenu = Enum.KeyCode.M
+
+-- Movement
+Keybinds.Dash = Enum.KeyCode.F
+Keybinds.FlyUp = Enum.KeyCode.Space
+Keybinds.FlyDown = Enum.KeyCode.LeftShift
+
+-- Aim Assist
+Keybinds.Aim = "RMB" -- RMB | Shift | Mouse4
+
+-- Teleport
+Keybinds.TeleportMouse = Enum.UserInputType.MouseButton2
+
+return Keybinds
 
