22635e09c1c0945947d1ebf74e2a45e9b898cad8
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:05:08 -0800

    Update fullbright.lua

--- Simple-Hub/visuals/fullbright.lua
+++ Simple-Hub/visuals/fullbright.lua
@@ -1,1 +1,26 @@
+-- visuals/fullbright.lua
+
+local Lighting = game:GetService("Lighting")
+
+local original = {
+	Ambient = Lighting.Ambient,
+	Brightness = Lighting.Brightness,
+	OutdoorAmbient = Lighting.OutdoorAmbient
+}
+
+_G.Hub.Fullbright = false
+
+function _G.Hub.ToggleFullbright(state)
+	_G.Hub.Fullbright = state
+
+	if state then
+		Lighting.Ambient = Color3.new(1,1,1)
+		Lighting.OutdoorAmbient = Color3.new(1,1,1)
+		Lighting.Brightness = 2
+	else
+		for k,v in pairs(original) do
+			Lighting[k] = v
+		end
+	end
+end
 
