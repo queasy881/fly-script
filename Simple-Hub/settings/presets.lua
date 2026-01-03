b6901dcad8ee98a6eaebe9a5888391b3f6ece830
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:19:01 -0800

    Update presets.lua

--- Simple-Hub/settings/presets.lua
+++ Simple-Hub/settings/presets.lua
@@ -1,1 +1,22 @@
+-- settings/presets.lua
+
+local Presets = {}
+
+Presets.Data = {
+	Legit = {},
+	Rage = {},
+	Movement = {},
+	Visual = {}
+}
+
+function Presets.save(name, state)
+	if not Presets.Data[name] then return end
+	Presets.Data[name] = table.clone(state)
+end
+
+function Presets.load(name)
+	return Presets.Data[name]
+end
+
+return Presets
 
