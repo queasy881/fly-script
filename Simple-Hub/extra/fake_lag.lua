18f4bf3acfef38a2ca6c5d891843849da4fcb461
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:24:03 -0800

    Update fake_lag.lua

--- Simple-Hub/extra/fake_lag.lua
+++ Simple-Hub/extra/fake_lag.lua
@@ -1,1 +1,22 @@
+-- extra/fake_lag.lua
+
+local FakeLag = {}
+
+FakeLag.enabled = false
+FakeLag.interval = 1
+local lastLag = 0
+
+function FakeLag.update(root)
+	if not FakeLag.enabled then return end
+	if tick() - lastLag < FakeLag.interval then return end
+
+	lastLag = tick()
+	local cf = root.CFrame
+	root.Anchored = true
+	task.wait(0.1)
+	root.Anchored = false
+	root.CFrame = cf
+end
+
+return FakeLag
 
