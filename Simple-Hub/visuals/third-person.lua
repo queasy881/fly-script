0a8072cac69d933644e822f469aa48418a86c596
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:06:07 -0800

    Update third-person.lua

--- Simple-Hub/visuals/third-person.lua
+++ Simple-Hub/visuals/third-person.lua
@@ -1,1 +1,18 @@
+-- visuals/third_person.lua
+
+local player = _G.Hub.Player
+
+_G.Hub.ThirdPerson = false
+
+function _G.Hub.ToggleThirdPerson(state)
+	_G.Hub.ThirdPerson = state
+
+	if state then
+		player.CameraMinZoomDistance = 15
+		player.CameraMaxZoomDistance = 100
+	else
+		player.CameraMinZoomDistance = 0.5
+		player.CameraMaxZoomDistance = 128
+	end
+end
 
