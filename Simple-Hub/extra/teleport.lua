f4cf1ab6656d3d8400190ccdf6b3987dc527dcb3
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:23:43 -0800

    Update teleport.lua

--- Simple-Hub/extra/teleport.lua
+++ Simple-Hub/extra/teleport.lua
@@ -1,1 +1,15 @@
+-- extra/teleport.lua
+
+local Teleport = {}
+
+Teleport.enabled = false
+
+function Teleport.toCursor(root, mouse)
+	if not Teleport.enabled then return end
+	if mouse.Hit and root then
+		root.CFrame = CFrame.new(mouse.Hit.Position + Vector3.new(0, 3, 0))
+	end
+end
+
+return Teleport
 
