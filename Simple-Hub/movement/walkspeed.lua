9132d5977994508dd33c5cc49096666f9566e6d4
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:20:51 -0800

    Update walkspeed.lua

--- Simple-Hub/movement/walkspeed.lua
+++ Simple-Hub/movement/walkspeed.lua
@@ -1,1 +1,17 @@
+-- movement/walkspeed.lua
+
+local WalkSpeed = {}
+
+WalkSpeed.enabled = false
+WalkSpeed.value = 16
+
+function WalkSpeed.apply(humanoid)
+	if WalkSpeed.enabled then
+		humanoid.WalkSpeed = WalkSpeed.value
+	else
+		humanoid.WalkSpeed = 16
+	end
+end
+
+return WalkSpeed
 
