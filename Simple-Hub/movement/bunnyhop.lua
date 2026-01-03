e633561bcb6b61aab9f73d2450ec216a8ec4b3a3
queasy881 <nicksidirop2009@gmail.com>
2026-01-03 02:21:29 -0800

    Update bunnyhop.lua

--- Simple-Hub/movement/bunnyhop.lua
+++ Simple-Hub/movement/bunnyhop.lua
@@ -1,1 +1,20 @@
+-- movement/bunnyhop.lua
+
+local BunnyHop = {}
+
+BunnyHop.enabled = false
+BunnyHop.delay = 0.2
+local lastHop = 0
+
+function BunnyHop.update(humanoid)
+	if not BunnyHop.enabled then return end
+	if tick() - lastHop >= BunnyHop.delay then
+		if humanoid.FloorMaterial ~= Enum.Material.Air then
+			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+			lastHop = tick()
+		end
+	end
+end
+
+return BunnyHop
 
